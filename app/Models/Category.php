23109c133f8149dafb5a3721dd3a81d5624553bf
deepakpraveen <deepakpraveen2001@gmail.com>
2025-12-11 10:02:54 +0530

    category

---
+++ app/Models/Category.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+
+      public function products()
+    {
+        return $this->hasMany(Product::class, 'category_id');
+    }
+
+    public function admin() {
+        return $this->hasOne(Admin::class, 'id', 'admin_id');
+    }
+}
