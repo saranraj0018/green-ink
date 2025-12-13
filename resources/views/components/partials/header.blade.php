a92eadecfb663d61727f7dd436648b2246854d45
saranraj0018 <saranrajhuk0018@gmail.com>
2025-12-13 11:29:16 +0530

    payment gateway

--- resources/views/components/partials/header.blade.php
+++ resources/views/components/partials/header.blade.php
@@ -1,5 +1,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <title>Greenink</title>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" />
 @vite(['resources/css/app.css', 'resources/js/app.js'])
